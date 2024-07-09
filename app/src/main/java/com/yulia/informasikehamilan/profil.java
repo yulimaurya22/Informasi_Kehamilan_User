3a6edbbb2edd2936bcfdc2f92b95fb9d28a06421
yulimaurya22 <yulimaurya22@gmail.com>
2024-07-09 19:12:34 +0700

    lanjutan edit layout

--- app/src/main/java/com/yulia/informasikehamilan/profil.java
+++ app/src/main/java/com/yulia/informasikehamilan/profil.java
@@ -1,15 +1,30 @@
 package com.yulia.informasikehamilan;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 public class profil extends AppCompatActivity {
+    ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
-        getSupportActionBar().hide();
+        getSupportActionBar().setTitle("Edit Profil");
+        actionBar = getSupportActionBar();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
     }
 }
